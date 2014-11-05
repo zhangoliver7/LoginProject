e076d8b6626802772551dadd558b87930f8b8ec4
Philipp Steinmann <steinmann.philipp@yahoo.com>
2014-11-04 23:11:40 -0500

    Add session library

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 from flask import Flask,request,redirect,render_template,session
-import db
+import sessions
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return redirect('home')
+    session = MongoSessionInterface()
+    #print session
+    return render_template("index.html")
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    pass
 
 @app.route('/register',methods=['GET','POST'])
 def register():
+    pass
 
 @app.route('/schedule')
 def schedule():
+    pass
 
 @app.route('/archive')
 def archive():
+    pass
          
+if __name__== "__main__":
+    app.run()
