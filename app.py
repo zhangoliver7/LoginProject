8e3c3f10fb4b703ede6db129a6bace2cdb992a87
Philipp Steinmann <steinmann.philipp@yahoo.com>
2014-11-05 20:41:02 -0500

    Make app.py skeleton

--- app.py
+++ app.py
@@ -1,29 +1,57 @@
-from flask import Flask,request,redirect,render_template,session
+from flask import Flask,request,redirect,render_template,session, flash
 import sessions
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    session = MongoSessionInterface()
-    #print session
     return render_template("index.html")
 
 @app.route('/login',methods=['GET','POST'])
 def login():
-    pass
+    if "uid" in session:
+        return redirect("/")
+
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        pass
+        # log the user in
 
 @app.route('/register',methods=['GET','POST'])
 def register():
-    pass
+    if "uid" in session:
+        return redirect("/")
+
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        pass
+        # register the user
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if not "uid" in session:
+        return redirect ("/")
+
+    if request.method == "GET":
+        return render_template("account.html")
+    if request.method == "POST":
+        pass
+        # update password
+
 
 @app.route('/schedule')
 def schedule():
-    pass
+    if "uid" in session:
+        return redirect("/")
 
-@app.route('/archive')
-def archive():
-    pass
-         
+    if request.method == "GET":
+        # fetch current schedule
+        return render_template("schedule.html")
+    if request.method == "POST":
+        pass
+        # update schedule
+
 if __name__== "__main__":
     app.run()
